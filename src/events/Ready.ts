127d6e67729a439a703b5bcc98fd459c0ec7519f
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2023-12-04 17:07:03 +0000

    Update discord.js version log

--- src/events/Ready.ts
+++ src/events/Ready.ts
@@ -2,7 +2,7 @@ import type { Client } from 'discordx';
 import { Discord, Once } from 'discordx';
 import si from 'systeminformation';
 import 'colors';
-import { ActivityType, ActivityOptions } from 'discord.js';
+import { ActivityType, ActivityOptions, version } from 'discord.js';
 
 /**
  * Discord.js Ready event handler.
@@ -72,7 +72,7 @@ export class Ready {
         );
         console.log(
             'Discord.js Version:'.white.bold,
-            `${process.env.npm_package_dependencies_discord_js?.substring(1)}`.green.bold,
+            `${version}`.green.bold,
         );
         console.log(
             `${client.user?.username} Version:`.white.bold,
