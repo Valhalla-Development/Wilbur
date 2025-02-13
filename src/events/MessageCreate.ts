5143ba0d9559baf9599a4142d8d998653279d025
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2025-02-13 16:12:21 +0000

    ✨ (MessageCreate.ts): add support for posting messages to Reddit with image attachments if Reddit posting is enabled and message is in specified channel

--- src/events/MessageCreate.ts
+++ src/events/MessageCreate.ts
@@ -14,8 +14,20 @@ export class MessageCreate {
         // Return if the author is a bot, preventing the bot from replying to itself or other bots.
         if (message.author.bot) return;
 
+        // Check if the message is in the specified channel and if Reddit posting is enabled
         if (process.env.REDDIT_POST === 'true' && (process.env.DISCORD_CHANNEL_ID && process.env.DISCORD_CHANNEL_ID === message.channel.id)) {
-            await postToReddit(client, message.content, message.author.displayName);
+            let imageUrl: string | undefined;
+
+            // Check for attachments and get the URL of the first image attachment
+            if (message.attachments.size > 0) {
+                const attachment = message.attachments.first();
+                if (attachment && attachment.url) {
+                    imageUrl = attachment.url; // Get the URL of the image
+                }
+            }
+
+            // Call postToReddit with the content and imageUrl if available
+            await postToReddit(client, message.content, message.author.displayName, imageUrl);
         }
     }
 }
